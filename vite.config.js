98900a0f542d0edff493d10f6de6aedad0fb91f9
Oluwatosin Abiodun <oluwatosin.sabiodun2006@gmail.com>
2026-07-11 16:27:13 +0100

    Add sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://www.chivicrose.com',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/services',
+        '/contact',
+      ],
+    }),
+  ],
 })
